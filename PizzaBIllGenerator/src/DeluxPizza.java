70b02b187bfb9d5dc2228e7f71ca2a06e02dc93a
sahil1148 <kadbe.sahil@gmail.com>
2023-07-16 14:58:46 +0530

    pizzabillingsystem
    committing pizzabillingsystem project

---
+++ PizzaBIllGenerator/src/DeluxPizza.java
@@ -0,0 +1,16 @@
+
+public class DeluxPizza extends Pizza{
+
+	public DeluxPizza(Boolean veg) {
+		super(veg);
+		super.addExtraCheese();
+		super.addExtraTopping();
+	}
+
+	@Override
+	public void addExtraCheese() {}
+
+	@Override
+	public void addExtraTopping() {}
+
+}
